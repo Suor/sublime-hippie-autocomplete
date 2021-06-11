f9c4af684a55c92f124ab602d6d2e3544c5768dc
Alexander Schepanovski <suor.web@gmail.com>
2021-06-11 17:10:08 +0700

    Initial version

---
+++ README.md
@@ -0,0 +1,20 @@
+# Hippie Word Completion
+
+Sublime Text 2/3 style auto completion for ST4: cycle through words, do not show popup. Simply hit `Tab` to insert completion, hit `Tab` again if you don't like it.
+
+Features:
+
+- fuzzy search
+- current view words go first
+
+TODO:
+
+- prefer matching first letters in combined_words
+- prioritize previously chosen completions
+- prefer words closer to cursor?
+- take into account scope/language word defined vs we are editing
+- obey `word_separators` setting to parse words
+- `shift+tab` to cycle back? `backspace` to cancel?
+
+
+Started from discussion [here](https://forum.sublimetext.com/t/st3-style-autocomplete-in-st4/57774) and based on sketch by **LightsOut8008**.
---
+++ hippie.py
@@ -0,0 +1,98 @@
+import sublime
+import sublime_plugin
+import re
+
+VIEW_TOO_BIG = 1000000
+WORD_PATTERN = re.compile(r'(\w{2,})', re.S)  # Start from words of length 2
+
+words_by_view = {}
+words_global = set()
+matching = []
+lookup_index = 0
+last_choice = ''
+
+
+class HippieWordCompletionCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        global last_choice, lookup_index, matching
+
+        primer_region = self.view.word(self.view.sel()[0])
+        primer = self.view.substr(primer_region)
+
+        def _matching(*sets):
+            for s in sets:
+                yield from fuzzyfind(primer, s)
+            yield primer  # Always be able to cycle back
+
+        if primer != last_choice:
+            lookup_index = 0
+            matching = list(distinct(_matching(words_by_view[self.view], words_global)))
+
+        if matching[lookup_index] == primer:
+            lookup_index += 1
+        if lookup_index >= len(matching):
+            lookup_index = 0
+        last_choice = matching[lookup_index]
+
+        self.view.replace(edit, primer_region, last_choice)
+
+
+class HippieListener(sublime_plugin.EventListener):
+    def on_init(self, views):
+        for view in views:
+            if view.size() > VIEW_TOO_BIG:
+                continue
+            contents = view.substr(sublime.Region(0, view.size()))
+            words_by_view[view] = words = set(WORD_PATTERN.findall(contents))
+            words_global.update(words)
+
+    def on_modified_async(self, view):
+        if view.size() > VIEW_TOO_BIG:
+            returnasd
+        text  = view.substr(sublime.Region(0, view.size()))
+        words_by_view[view] = words = set(WORD_PATTERN.findall(text))
+        words_global.update(words)
+
+
+def fuzzyfind(primer, collection, sort_results=True):
+    """
+    Args:
+        primer (str): A partial string which is typically entered by a user.
+        collection (iterable): A collection of strings which will be filtered
+                               based on the `primer`.
+        sort_results(bool): The suggestions are sorted by considering the
+                            smallest contiguous match, followed by where the
+                            match is found in the full string. If two suggestions
+                            have the same rank, they are then sorted
+                            alpha-numerically. This parameter controls the
+                            *last tie-breaker-alpha-numeric sorting*. The sorting
+                            based on match length and position will be intact.
+    Returns:
+        suggestions (generator): A generator object that produces a list of
+            suggestions narrowed down from `collection` using the `primer`.
+    """
+    suggestions = []
+    pat = '.*?'.join(map(re.escape, primer))
+    pat = '(?=({0}))'.format(pat)   # lookahead regex to manage overlapping matches
+    regex = re.compile(pat, re.IGNORECASE)
+    for item in collection:
+        if item == primer:
+            continue
+        r = list(regex.finditer(item))
+        if r:
+            best = min(r, key=lambda x: len(x.group(1)))   # find shortest match
+            suggestions.append((len(best.group(1)), best.start(), item, item))
+
+    if sort_results:
+        return [z[-1] for z in sorted(suggestions)]
+    else:
+        return [z[-1] for z in sorted(suggestions, key=lambda x: x[:2])]
+
+
+def distinct(seq):
+    """Iterates over sequence skipping duplicates"""
+    seen = set()
+    for item in seq:
+        if item not in seen:
+            seen.add(item)
+            yield item
